98f10e6bdf5d46ca392b0144c4254088e994e6d9
David-Martinez02 <dmart0101@jaguar.tamu.edu>
2022-07-27 17:44:04 -0500

    EJS transformation
    converted all necessary files to support ejs. Now in order to view the pages, use terminal and type 'node server.js' . liveserver does not seem to work anymore, but this is what we needed to do for the presentation on thursday.

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require("body-parser")
+const ejs = require('ejs')
+
+const app = express()
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
+
+const port = 3000
+
+app.get('/', (req, res) => {
+res.render('index');
+})
+
+app.get('/vanity', (req, res) => {
+    res.render('vanity');
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
+
+app.get('/cart', (req, res) => {
+    res.render('cart');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
